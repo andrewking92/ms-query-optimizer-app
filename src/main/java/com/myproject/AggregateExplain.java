38a0b5fdf92d6d97b0d706e3192dc82bb91e3614
Andrew King <kingan916@gmail.com>
2023-02-14 10:10:13 +0000

    aggregation explain

---
+++ src/main/java/com/myproject/AggregateExplain.java
@@ -0,0 +1,44 @@
+package com.myproject;
+
+import static com.mongodb.client.model.Filters.eq;
+
+import org.bson.Document;
+import java.util.*;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Aggregates;
+
+
+public class Aggregate {
+    public static void main( String[] args ) {
+
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "";
+
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("ms_test");
+
+            MongoCollection<Document> collection = database.getCollection("party");
+
+            Document explanation = collection.aggregate(
+                Arrays.asList(
+                        Aggregates.match(eq("partyID", "BC-854765140862566414")),
+                        Aggregates.group("$stars", Accumulators.sum("count", 1))
+                )
+            ).explain(ExplainVerbosity.EXECUTION_STATS);
+            
+            List<Document> stages = explanation.get("stages", List.class);
+            List<String> keys = Arrays.asList("queryPlanner", "winningPlan");
+
+            for (Document stage : stages) {
+                Document cursorStage = stage.get("$cursor", Document.class);
+                if (cursorStage != null) {
+                    System.out.println(cursorStage.getEmbedded(keys, Document.class).toJson());
+                }
+            }
+        }
+    }
+}
